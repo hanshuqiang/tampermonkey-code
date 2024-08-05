833bc3012d7b81eae1acf7f638d92c2c6eec27f0
hanshuqiang <hanshuqiang92@gmail.com>
2024-08-06 03:38:56 +0800

    Update steam_buy_order.js

--- steam_buy_order.js
+++ steam_buy_order.js
@@ -6,17 +6,22 @@
 // @author       You
 // @match        https://steamcommunity.com/market/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=steamcommunity.com
-// @grant        none
+// @grant        GM_notification
+// @grant       GM.getResourceText
+// @grant       GM.xmlHttpRequest
+// @resource myTxt1 file:///D:/data1.csv
+// @resource myTxt file:///D:/data.csv
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-        // 在页面上添加一个按钮
+    // 在页面上添加一个按钮
     var button = document.createElement("button");
     button.id = "exportButton";
     button.textContent = "导出";
     button.style.padding = "5px 10px";
+    button.style.margin = " 0 5px 0 0 ";
     button.style.backgroundColor = "#4CAF50";
     button.style.color = "white";
     button.style.border = "none";
@@ -24,8 +29,21 @@
     button.style.cursor = "pointer";
     document.querySelectorAll('.market_home_listing_table')[1].querySelector('.my_market_header').appendChild(button);
 
+    var button2 = document.createElement("button");
+    button2.id = "exportButto2n";
+    button2.textContent = "导入";
+    button2.style.padding = "5px 10px";
+    button2.style.backgroundColor = "#4CAF50";
+    button2.style.color = "white";
+    button2.style.border = "none";
+    button2.style.borderRadius = "4px";
+    button2.style.cursor = "pointer";
+    document.querySelectorAll('.market_home_listing_table')[1].querySelector('.my_market_header').appendChild(button2);
+
+
     // 绑定按钮点击事件
     document.getElementById("exportButton").addEventListener("click", exportTableToCSV);
+    document.getElementById("exportButto2n").addEventListener("click", uploadfile);
 
     function exportTableToCSV(){
         let mybuyorder = document.querySelectorAll('.market_home_listing_table')[1]
@@ -53,8 +71,41 @@
         link.setAttribute("download", "table_data.csv");
         document.body.appendChild(link);
         link.click();
+
         document.body.removeChild(link);
+        GM_notification({
+            text: "导出成功，请在下载文件中查看",
+            title: "提示",
+            timeout: 3000, // 消失时间, 单位毫秒
+            onClick: function() {
+                console.log("用户点击了提示");
+            }
+        });
+    }
+    function convertCSVtoArray(csvData) {
+        var lines = csvData.trim().split("\n");
+        var result = [];
+
+        for (var i = 0; i < lines.length; i++) {
+            var cells = lines[i].split(",");
+            result.push(cells);
+        }
+
+        return result;
+    }
+    function uploadfile(){
+        // 读取 CSV 文件
+        GM.getResourceText("myTxt1")
+            .then(csvData => {
+            let arr = convertCSVtoArray(csvData);
+            console.log("CSV data:", arr);
+
+        })
+            .catch(error => {
+            console.error("Error reading CSV:", error);
+        });
+
+
     }
 
- 
 })();
